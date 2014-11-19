393981a08a5abdbdcf4202ef764e256fdfb6bbdb
Geramy <geramy92@users.noreply.github.com>
2014-11-19 15:41:12 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- not used in all files... will be set later
+config={}
+
+-- array with all domains which should not found in Anti-Advertise-Functions
+config["domains"]={"terratex.eu","tt-rl.de"} 
+
+-- Main Domain / will later be set in credits or info guis
+config["maindomain"]="terratex.eu"
+
+
+

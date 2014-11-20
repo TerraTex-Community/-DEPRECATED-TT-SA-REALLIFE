df3a9a909c11c9891e120c8bb8dd6064669f14ae
Geramy <geramy92@users.noreply.github.com>
2014-11-20 14:12:56 +0100

    Update config.lua
    Attention a lot of the new Settings are not implemented yet

--- config.lua
+++ config.lua
@@ -27,4 +27,20 @@ config["mappername"]="[TTeam]Blizard"
 --Scriptername
 config["scriptername"]="[TTeam]Geramy92 und [TTeam]Maxx"
 
+--MYSQL CONFIG:
+config["mysqlhost"]="localhost"
+config["mysqluser"]="username"
+config["mysqlpassword"]="password"
+config["mysqldb"]="databasename"
+
+--IF You want to have a second unique log database set 'uniquelogdb' to true else let it be false
+--IF true setup the other entries
+config["uniquelogdb"]=false
+config["logmysqlhost"]="localhost"
+config["logmysqluser"]="logusername"
+config["logmysqlpassword"]="logpassword"
+config["logmysqldb"]="logdatabasename"
+
+
+
 
